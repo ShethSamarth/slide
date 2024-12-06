cb19947889f4cd31b29d5bc0f126583cfe477fda
Samarth Sheth <samarth.sheth.29@gmail.com>
2024-12-06 21:21:30 +0530

    landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
 
+import { ThemeProvider } from "@/providers/theme-provider"
+
 import "./globals.css"
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] })
@@ -12,8 +14,17 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body className={jakarta.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={jakarta.className}>
+        <ThemeProvider
+          attribute="class"
+          forcedTheme="dark"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
